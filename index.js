135569909f9ac40fe88512aabaff95e642936e7a
Ernesto García <ernt26dev@gmail.com>
2020-07-27 08:26:19 -0500

    beta version with languages

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = function() {
-  console.log('Hello Mars!');
+module.exports = function(lang) {
+  if (lang === "es") {
+    console.log('Hola Saturno!!');
+  } else {
+    console.log('Hello Saturn!!');
+  }
 }
